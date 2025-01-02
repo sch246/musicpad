f3f1f6c884fd94092427d6d2131ae00af91c6fe1
sch246 <980001119@qq.com>
2025-01-02 22:23:13 +0800

    w
    添加了图标
    添加了exe文件生成

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import sys
+import os
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QIcon
 from musicpad import AudioPlayer
 
+def resource_path(relative_path):
+    """获取资源绝对路径"""
+    if hasattr(sys, '_MEIPASS'):
+        # PyInstaller会创建临时文件夹并定义_MEIPASS
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
 
 def main():
+    icon_path = resource_path("icon.ico")
     app = QApplication(sys.argv)
     player = AudioPlayer()
+    player.setWindowIcon(QIcon(icon_path))
     player.show()
     sys.exit(app.exec())
 
